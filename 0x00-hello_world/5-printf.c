7fea90597149e5656d0a09b5dcda2edd1441e14c
Damilola Ifabiyi <damilolamary39@gmail.com>
2023-04-20 16:30:46 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
-
 /**
- * main - Prints "with proper grammer, but the outcome is
- * a piece of art, " followed newline
- * Return: Always 0.
+ * main - Entry point
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
-
 {
 	printf("with proper grammer, but the outcome is a place of art,\n");
-	return(0):
+	return(0);
 }	
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <studio.h>
 
+
 /**
  * main - Prints the size of various types based on 
  * the computer it is compiled run on.
@@ -10,8 +11,10 @@ int main(void)
 
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof (long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof (long long long int));
+	printf("Size of a float %zu byte(s)\n", sizeof (float));
 	return (0);
 }	
