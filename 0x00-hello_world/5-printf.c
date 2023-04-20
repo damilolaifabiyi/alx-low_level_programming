5eadd10fbc0780a51e7ff2b441e14e48c09515c4
Damilola Ifabiyi <damilolamary39@gmail.com>
2023-04-20 15:27:19 -0700

    print fuction

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print "entry point"
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a place of art,\n");
+	return(0);
+}	
