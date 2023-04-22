8eff07a6821e7b5e66658a2770acf6da59d6b419
Damilola Ifabiyi <damilolamary39@gmail.com>
2023-04-22 06:07:32 -0700

    print bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,4 @@
-#include <studio.h>
-
-
+#include <stdio.h>
 /**
  * main - Entry point
  * Description: Print the size of bytes.
@@ -8,12 +6,12 @@
  */
 
 int main(void)
-
 {
-	printf("Size of a char: %li byte(s)\n", sizeof(char));
-	printf("Size of an int: %li byte(s)\n", sizeof(int));
-	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %li byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %li byte(s)\n", sizeof(float));
+	printf("Size of a char: %li bytes(s)\n", sizeof(char));
+	printf("Size of a int: %li bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %li bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %li bytes(s)\n", sizeof(float));
+
 	return (0);
 }
