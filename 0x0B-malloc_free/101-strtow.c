e7f3b3887e7ab07a87a7f2c96a5b9c6340934cab
Damilola Ifabiyi <damilolamary39@gmail.com>
2023-06-13 15:41:24 -0700

    fix error

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -53,25 +53,25 @@ char **strtow(char *str)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
-		if (c)	
+			if (c)
 			{
-					end = i;
-					tmp = (char *) malloc(sizeof(char) * (c + 1));
-					if (tmp == NULL)
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
 					return (NULL);
-					while (start < end)
+				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
 				c = 0;
-		}
+			}
 		}
 		else if (c++ == 0)
-		start = i;
+			start = i;
 	}
 
 	matrix[k] = NULL;
 
 	return (matrix);
-
+}
