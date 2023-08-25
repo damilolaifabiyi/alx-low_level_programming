9045cf9b4924baf707fd0838da442bfe51df08bc
Damilola Ifabiyi <damilolamary39@gmail.com>
2023-08-25 16:26:11 -0700

    fix error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/damilolaifabiyi/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
